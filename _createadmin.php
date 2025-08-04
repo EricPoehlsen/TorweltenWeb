12d0fb74fa6a5be705fe62ead403ade99d5944b4
eric <eric@eric-poehlsen.de>
2025-08-04 19:35:33 +0200

    Check if passwords match
    on registering the admin user.

--- _createadmin.php
+++ _createadmin.php
@@ -4,15 +4,23 @@
     include "config.php";
     $dsn = "mysql:dbname=$db_name;host=$db_serv";
     $db = new PDO($dsn, $db_user, $db_pass);
+    $error = "";
     
     //handle $_POST to create owner account in database
     if(array_key_exists("user", $_POST)) {
-        $user = $_POST['user'];
-        $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-        $mail = $_POST['mail'];
-        $sql = "INSERT INTO users (username, passhash, mail, access) VALUES (?, ?, ?, 99)";
-        $insert = $db->prepare($sql);
-        $insert->execute([$user, $pass, $mail]);
+        //check if passwords match
+        if ($_POST["pass"] == $_POST["pchk"]) {
+            $user = $_POST['user'];
+            $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+            $mail = $_POST['mail'];
+            $sql = "INSERT INTO users (username, passhash, mail, access) VALUES (?, ?, ?, 99)";
+            $insert = $db->prepare($sql);
+            $insert->execute([$user, $pass, $mail]);
+        } else {
+            $error = "Passwörter stimmen nicht überein.";
+        }
+
+
     } 
 
     //display form only if owner does not yet exist. 
@@ -26,13 +34,14 @@
         <p>Passwort: <input name="pass" type="password"/></p>
         <p>Passwort: <input name="pchk" type="password"/></p>
         <input type="Submit" value="Registrieren"/>
+        <p><span style="color:'#f00'"><?php echo $error;?></span></p>
         </form>
         <?php
     } else {
         $owner = $admin->fetchAll()[0]["username"];
         ?>
         <h1>Besitzerkonto angelegt</h1>
-        <p>Der Benutzer <i><?php echo $owner; ?> wurde als Besitzerkonto im System registriert.</p>
+        <p>Der Benutzer <i><?php echo $owner; ?></i> wurde als Besitzerkonto im System registriert.</p>
         <p><a href="install.php">Weiter ...</a></p>
         <?php
     }
