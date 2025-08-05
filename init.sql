8ede5622ac58b8b5f165b8caf891fee0ff980d7b
Eric Pöhlsen <eric@eric-poehlsen.de>
2025-08-05 16:10:45 +0200

    Preparing the Database for characters
    This is, what this site is about. Creating characters for Torwelten. Added the basic data for characters to the database definition

--- init.sql
+++ init.sql
@@ -15,3 +15,22 @@ CREATE TABLE logins (
     token varchar(255) NOT NULL,
     expires TIMESTAMP
 );
+
+CREATE TABLE characters {
+    charid int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userid int NOT NULL,
+    charname varchar(255),
+    species varchar(255),
+    concept varchar(255),
+    phy tinyint DEFAULT 0,
+    men tinyint DEFAULT 0,
+    soz tinyint DEFAULT 0,
+    nk tinyint DEFAULT 0,
+    fk tinyint DEFAULT 0,
+    lp tinyint DEFAULT 0,
+    ep tinyint DEFAULT 0,
+    mp tinyint DEFAULT 0,
+    cur_lp decimal(3,1) DEFAULT 0.0,
+    cur_ep decimal(3,1) DEFAULT 0.0,
+    cur_mp decimal(3,1) DEFAULT 0.0
+};
