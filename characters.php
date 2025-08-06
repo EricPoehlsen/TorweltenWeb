ae61e9fa2a6c5731744618df8a024820715a8fe6
eric <eric@eric-poehlsen.de>
2025-08-06 04:08:31 +0200

    implements character list

--- characters.php
+++ characters.php
@@ -4,14 +4,34 @@ ini_set('display_errors', 1);
 
 // This page shows all characters of a user
 
+    // connect DB
+    include "config.php";
+    $dsn = "mysql:dbname=$db_name;host=$db_serv";
+    $db = new PDO($dsn, $db_user, $db_pass);
+
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title></title>
+<head>
+    <title></title>
 </head>
 <body>
     <div id="userbar"><?php include "_userbar.php"; ?></div>
-    <p>Hallo</p>
+    <h1>Charaktere</h1>
+    <?php 
+        //get characters from database
+        $sql = "SELECT charid, charname, species, concept FROM characters WHERE userid = ?";
+        $stmt = $db->prepare($sql);
+        $stmt->execute([intval($_SESSION["userid"])]);
+        $characters = $stmt->fetchAll();
+        //display characters
+        foreach ($characters as $c) {
+            echo "<p><a href='character.php?id={$c['charid']}'>{$c['charname']}</a> - {$c['species']} - {$c['concept']}</p>";
+        }
+    ?>
+
+
+
+
 </body>
 </html>
