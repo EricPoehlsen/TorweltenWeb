dbaf1b7ab0d3b1c46fd91ea95c28c0816d0b511e
eric <eric@eric-poehlsen.de>
2025-08-19 04:25:01 +0200

    Logging XP cost of added trait

--- addchartrait.php
+++ addchartrait.php
@@ -25,9 +25,19 @@ include "_checklogin.php";
         // insert trait into char traits ...
         if (count($data) > 0) {
             $trait = $data[0];
-            $sql = "INSERT INTO chartraits (charid, title, tdesc, maxrank, xpcost, traittype) VALUE (?, ?, ?, ?, ?, ?)";
+            $sql = "INSERT INTO chartraits (charid, title, tdesc, maxrank, xpcost, traittype) VALUES (?, ?, ?, ?, ?, ?)";
             $stmt = $db->prepare($sql);
             $stmt->execute([$charid, $trait["title"], $trait["tdesc"], $trait["maxrank"], $trait["xpcost"], $trait["traittype"]]);
+            
+            // log xp
+            $userid = $_SESSION["userid"];
+            $reason = "Eigenschaft {$trait['title']} hinzugefügt";
+            $sql = "INSERT INTO xplog (charid, userid, xp, reason) VALUES (?, ?, ?, ?)";
+            $stmt = $db->prepare($sql);
+            $stmt->execute([$charid, $userid, $trait["xpcost"], $reason]);
+
             echo "1";
+        } else {
+            echo "0";
         }
     }
